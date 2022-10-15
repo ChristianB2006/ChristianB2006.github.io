b92c2f48011ad9ac682a67623296c55c5bcfdd60
ChristianB2006 <crb0200023@lpssonline.com>
2022-10-15 18:07:14 +0000

    Final

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -28,7 +28,7 @@
 
     ////////////////////////////////////////////////////////////////////////
     // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-
+    
     /*
      * ground : here, we create a floor. Given the width of of the platform
      * asset, giving it a scaleX and scaleY of 2 will stretch it across the
@@ -36,10 +36,12 @@
      */
     createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
 
-    // example:
-    createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
-
+    
+    createPlatform(500, 300);
+    createPlatform(235, 520, 0.5);
+    createPlatform(600, 600, 0.5);
+    createPlatform(-100, 430, 0.6);
+    createPlatform(235, 350, 0.5);
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
