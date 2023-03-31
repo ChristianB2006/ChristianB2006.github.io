2cbe08ffc643a9ea235ec9d7bd91f7b4a07ed27b
ChristianB2006 <crb0200023@lpssonline.com>
2023-03-31 12:58:22 +0000

    continueLater

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,11 +36,25 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,475,'groundY');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
             
+
+            for (var starfield = 1; starfield < 400; starfield++) {
+            var circle = draw.circle(1, "white", "LightGray", 2);
+            circle.x = canvasWidth * Math.random();
+            circle.y = groundY * Math.random();
+            background.addChild(circle);
+            };
+            
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = 800;
+            moon.y = -100;
+            moon.scaleX = 0.5;
+            moon.scaleY = 0.5;
+            background.addChild(moon);
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
--- projects/runtime/js/view/hud.js
+++ projects/runtime/js/view/hud.js
@@ -9,7 +9,7 @@
     
     /*
 
-    Create a heads-up display for our game showing a scoreand an 
+    Create a heads-up display for our game showing a score and an 
     "integrity meter" which indicates our health. The returned object is a 
     create.js container which can be added to our game using the following code:
       
@@ -34,14 +34,14 @@
         hud = new createjs.Container();
         
         integrity = new createjs.Container();
-        background = draw.rect(104, 20, '#CCC');
+        background = draw.rect(104, 20, 'red');
         draw.rect(102, 18, '#FFF', null, null, 1, 1, background);
         integrity.addChild(background);
         
-        txtScore = draw.textfield('score : 000', "19px Arial", '#666', 'left');
+        txtScore = draw.textfield('score : 000', "20px Arial", 'white', 'left');
         hud.addChild(txtScore);
         
-        integrityMeter = draw.rect(1, 16, '#3333CC');
+        integrityMeter = draw.rect(1, 16, 'red');
         integrityMeter.scaleX = 100;
         integrity.addChild(integrityMeter);
         hud.addChild(integrity);
