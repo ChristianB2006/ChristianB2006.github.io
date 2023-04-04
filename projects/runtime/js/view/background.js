99696ec9b6b47619b9f0d57f4df81dd312503369
ChristianB2006 <crb0200023@lpssonline.com>
2023-04-04 12:54:19 +0000

    continue

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -27,6 +27,7 @@ var background = function (window) {
         
         // ANIMATION VARIABLES HERE:
         var tree = draw.bitmap("img/tree.png");
+        var buildings = []
      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -57,7 +58,14 @@ var background = function (window) {
             background.addChild(moon);
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for (var i = 0; i < 5; ++i) {
+                var buildingHeight = 300;
+                var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
             
             // TODO 4: Part 1 - Add a tree
             tree = draw.bitmap("img/tree.png");
@@ -84,7 +92,11 @@ var background = function (window) {
             }
             
             // TODO 5: Part 2 - Parallax
-            
+            for (var i = 0; i < buildings.length; i++) {
+                var buildings = buildings[i];
+              
+                // code to do something with each element
+              }
 
         } // end of update function - DO NOT DELETE
         
