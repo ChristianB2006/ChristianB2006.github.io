77c28022e4f2fdeea8103e218512dea3f877abf0
ChristianB2006 <crb0200023@lpssonline.com>
2023-08-30 12:43:06 +0000

    cont

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
     if (currentShape.color === "red") {
       currentShape.goodBehavior = "bounce"
     }
-    else if (currentShape.color === "blue") {
+    if (currentShape.color === "blue") {
       currentShape.goodBehavior = "blink"
     }
     else {
@@ -73,8 +73,8 @@ $(document).ready(function () {
 
   // TODO 4-a: add a function that handles the good display type
   function handleGood(color, shape, repeat) {
-    setBackgroundWithSimple(color, shape, repeat);
-    animationDetails.displayType = 2;
+    setBackgroundWithSimple(color, shape, repeat)
+    animationDetails.displayType = 2
   }
 
   // TODO 5-a: add a function that handles the bad display type
@@ -95,15 +95,15 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    currentShape = dataShapes[currentIndex]
+    var currentShape = dataShapes[currentIndex]
     handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
 
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    currentShape = dataShapes[currentIndex]
-    repeat = currentShape[repeat]
+    var currentShape = dataShapes[currentIndex]
+    var repeat = currentShape[repeat]
     handleBad(currentShape, repeat)
   }
 
